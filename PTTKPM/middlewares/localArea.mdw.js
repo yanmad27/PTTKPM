8aa5ca441c11e3e5cb309caa4014a7439e32dcf0
DOÃN PHAN NGUYỄN KHẮC <1612108@student.hcmus.edu.vn>
2019-06-11 20:39:59 +0700

    Tạo App

--- PTTKPM/middlewares/localArea.mdw.js
+++
@@ -1,7 +0,0 @@
-var areaModel = require('../models/area.model');
-module.exports = (rep, res, next) => {
-    areaModel.allWithDetail().then(rows => {
-        res.local.lcAreas = rows;
-        next();
-    });
-}
